5814e239d3348719652f66a0ca9b49f8d639222f
Pavashlin <pavashlingovender@yahoo.com>
2015-04-13 23:07:26 +0200

    comments

--- Question3.cpp
+++ Question3.cpp
@@ -22,6 +22,60 @@ private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction(int n, int d) : num(n), denom(d) { };
 	void print() { cout << num << "/" << denom; };
+	
+	friend bool operator>(Fraction a, Fraction b);												//makes it easier to create functions
+	friend Fraction add(int, Fraction);
+	friend Fraction add(Fraction, int);
 };
+
+bool operator>(Fraction a, Fraction b)
+{
+	if (a.num*b.denom > b.num*a.denom)
+	{
+		return true;
+	}
+	else return false;
+}
+
+ Fraction add(int x, Fraction y)
+{
+	 int temp;
+	 temp = x*y.denom;
+	 y.num = y.num + temp;
+	 return y;
+}
+
+ Fraction add(Fraction x, int y)
+ {
+	 int temp;
+	 temp = y*x.denom;
+	 x.num = x.num + temp;
+	 return x;
+ }
+
+ int main()
+ {
+	 Fraction Jack(10, 9), Jill(1, 2);
+	 int FatPenguin = 42;
+
+	 Fraction hurry(1, 1), up(1, 1);
+
+	 if (Jack > Jill)
+	 {
+		 cout << "It Works!" << endl;										//Jack is larger than Jill therefore if operator works the message "It works!" will be printed
+	 }
+	 else
+	 {
+		 cout << "Im a sad panda" << endl;									//prints im a sad panda if something goes wrong or if jill somehow becomes smaller than jack.
+	 }
+	
+	 hurry = add(FatPenguin, Jill);
+	 up = add(Jack, FatPenguin);
+
+	 hurry.print();
+	 cout << endl;
+	 up.print();
+
+ }
